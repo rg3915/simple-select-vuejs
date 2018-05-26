ee7d0c93aa37d1e1ff9405bcf53cf5367e330625
regis <regis.santos.100@gmail.com>
2018-05-25 22:32:43 -0300

    Fix Preview items OK

--- main.js
+++ main.js
@@ -1,53 +1,59 @@
 var app = new Vue({
   el: '#app',
   data: {
-    all_items: [],
+    items: [],
+    preview_items: [],
     selected: [],
-    selected2: [],
-    selected_items: [],
-    selectAll: false,
+    selected_preview: [],
+    selected_all: false,
+    selected_all_preview: false,
   },
   methods: {
     list_items(){
       axios.get('https://api.github.com/repos/rg3915/django-experience/issues')
       .then((result) => {
-        this.all_items = result.data.map((item) => {
+        this.items = result.data.map((item) => {
           return {number: item.number, id: item.id, title: item.title, state: item.state, url: item.url}
         })
       })
     },
-    addToPreview(){
-      // Copia os itens selecionados para a outra lista.
-      this.selected_items = this.all_items.filter(item => item.selected)
-      // Desativa a seleção da lista principal.
-      // this.selected_items.map(item => {
-      //   item.selected = false
-      // })
-    },
     removeFromPreview(){
       window.alert('Exercicio pro Regis')
     },
-    select(){
+    selectAll(){
       this.selected = [];
-      this.selected_items = [];
-      if (!this.selectAll) {
-        for (let i in this.all_items) {
-          this.item.selected.push(this.items[i].number);
+      this.preview_items = [];
+      if (!this.selected_all) {
+        for (let i in this.items) {
+          // Insere os números no array selected
+          // usado para saber quais os itens que foram selecionados.
+          this.selected.push(this.items[i].number);
         }
-        // for (let j in this.all_items) {
-        //   this.selected_items.push(
-        //     {number: this.all_items[j].number, id: this.all_items[j].id, title: this.all_items[j].title, state: this.all_items[j].state, url: this.all_items[j].url}
-        //   );
-        // }
+        // Uma lista é cópia da outra.
+        this.preview_items = this.items
+        this.selected_all_preview = true
+      }
+    },
+    selectAllPreview(){
+      this.selected_preview = [];
+      if (!this.selected_all_preview) {
+        for (let i in this.preview_items) {
+          // Insere os números no array selected_preview
+          // usado para saber quais os itens que foram selecionados.
+          this.selected_preview.push(this.preview_items[i].number);
+        }
       }
     }
   },
   watch: {
-    // selected(value){
-    //   this.selected_items = this.items.filter(
-    //     i => value.includes(i.number)
-    //   );
-    // }
+    // Copia os itens para a segunda lista um a um.
+    selected(value){
+      this.preview_items = this.items.filter(
+        i => value.includes(i.number)
+      );
+      // Ao abrir o modal os itens estão ticados.
+      this.selected_preview = this.selected
+    }
   },
   mounted(){
     this.list_items()
